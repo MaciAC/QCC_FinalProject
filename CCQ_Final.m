b284aee34cf972df72fea0ab3691aec4286a7615
MaciAC <macia.ac@gmail.com>
2022-01-29 20:49:26 +0100

    I(X;Y) works I(X;B) no

--- CCQ_Final.m
+++ CCQ_Final.m
@@ -1,3 +1,4 @@
+%% Plot I(X;B)
 syms theta
 
 angle = theta/2;
@@ -6,15 +7,29 @@ Qs_1 = [0; 1];
 theta_0 = [cos(angle); sin(angle)];
 theta_1 = [cos(angle); -sin(angle)];
 
-rho_XB = 1/2 * kron(Qs_0*Qs_0', theta_0*theta_0') + 1/2*kron(Qs_1*Qs_1', theta_1*theta_1');
+rho_XB = 1/2 * kron(Qs_0*Qs_0', theta_0*theta_0') + 1/2 * kron(Qs_1*Qs_1', theta_1*theta_1');
 
 rho_B = ptrace(rho_XB,1,[2,2]);
 
 H_rho_B = Q_entropy_2D(rho_B);
 
-I_XB = 1/2 * (H_rho_B - Q_entropy_2D(rho_B^0));
+
+
+I_XB = H_rho_B - 1/2*(Q_entropy_2D(theta_0*theta_0') + Q_entropy_2D(theta_1*theta_1'));
 
 fplot(I_XB, [0, pi])
+
+hold on
+
+xticks([0, pi/2, pi])
+xticklabels({'0', '\pi/2', '\pi'})
+
+%% Plot I(X;Y)
+P_e = 0.5*(1-sin(theta));
+H_P = -P_e*log(P_e) - (1-P_e)*log(1-P_e);
+I_Y = 1 - H_P;
+
+fplot(I_Y, [0, pi])
 
 hold on
 
--- Q_entropy_2D.m
+++ Q_entropy_2D.m
@@ -1,5 +1,11 @@
 function H = Q_entropy_2D(M)
     [v_B, val] = eig(M);
     p_B = [val(1,1) val(2,2)];
-    H = -trace( p_B(1)*log2(p_B(1))*v_B(:,1)*v_B(:,1)' + p_B(2)*log2(p_B(2))*v_B(:,2)*v_B(:,2)');
+    A = zeros(2);
+    for i = [1,2]
+        if p_B(i) ~= 0
+            A = A + p_B(i)*log2(p_B(i))*v_B(:,i)*v_B(:,i)';
+        end
+    end
+    H = -trace(A);
 end
